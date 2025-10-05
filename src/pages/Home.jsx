45bf2d3579692e3ce50208ffbe81581767c3c42c
KRISH <142862565+shah0108@users.noreply.github.com>
2025-10-06 01:46:29 +0530

    Update src/pages/Home.jsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,9 +110,10 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <br></br>
-      <br></br>
-      <br></br>
+      {/* Stats Section */}
+      <section className="bg-[#0b1220] py-12 mt-12">
+        {/* …rest of stats section… */}
+      </section>
       {/* Stats Section */}
       {/* Stats Section */}
 <section className="bg-[#0b1220] py-12">
